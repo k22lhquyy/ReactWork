aa959dac946c0a0b66013b9c2fd846124f6faaf6
quy le <huuquyle9@gmail.com>
2025-01-05 02:41:40 +0700

    5.1.2025

--- Project01/my-project/src/App.jsx
+++ Project01/my-project/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './components/Home'
 import Navbar from './components/Navbar'
 import { useEffect, useState } from 'react'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
     </>
   )
 }
--- Project01/my-project/src/components/Navbar.jsx
+++ Project01/my-project/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-scroll";
+import { FaXmark, FaBars } from "react-icons/fa6";
 
 
 const Navbar = () => {
@@ -38,8 +39,8 @@ const Navbar = () => {
   return (
     
     <header className="fixed top-0 left-0 right-0 w-full bg-white md:bg-transparent">
-      <nav>
-        <div>
+      <nav className="py-4 px-4 lg:px-12 ${sticky ? }">
+        <div className="flex justify-between items-center text-base gap-8">
           <a
             href=""
             className="flex items-center space-x-3 text-2xl font-semibold"
@@ -54,7 +55,28 @@ const Navbar = () => {
 
           <ul className="hidden space-x-12 md:flex">
             {
-              navIteams.map(({link, path}) => <Link to={path} spy={true} smooth={true} offset={-100} key={path}>{link}</Link>)
+              navIteams.map(({link, path}) => <Link to={path} spy={true} smooth={true} offset={-100} key={path} 
+              className="block text-base text-gray-900 hover:text-green-300 first:font-medium">{link}</Link>)
+            }
+          </ul>
+
+          <div className="space-x-12 hidden lg:flex items-center">
+            <a href="" className="hidden lg:flex items-center text-green-500 hover:text-gray-900">Login</a>
+            <button className="bg-green-400 text-white py-2 px-4 transition-all duration-300 rounded hover:bg-gray-700">Sign up</button>
+          </div>
+
+
+          <div className="md:hidden">
+            <button className="focus:outline-none focus:text-gray-400 text-neutral-600" onClick={toggleMenu}>
+              {isMenuOpen ? <FaXmark className="h-6 w-6 " /> : <FaBars className="h-6 w-6 " />}
+            </button>
+          </div>
+        </div>
+        <div>
+          <ul className={`${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"} md:hidden px-4 mt-16 py-7 bg-green-400`}>
+            {
+              navIteams.map(({link, path}) => <Link to={path} spy={true} smooth={true} offset={-100} key={path} 
+              className="block text-base text-gray-900 hover:text-green-300 first:font-medium">{link}</Link>)
             }
           </ul>
         </div>
