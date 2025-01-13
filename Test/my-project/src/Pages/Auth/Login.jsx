6182120ff3f6f97f0d838399c3a9f0b52f094133
quy le <huuquyle9@gmail.com>
2025-01-14 01:30:12 +0700

    14.1.2025

--- Test/my-project/src/Pages/Auth/Login.jsx
+++ Test/my-project/src/Pages/Auth/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PasswordInput from '../../components/Input/PasswordInput'
+import { useNavigate } from 'react-router-dom';
+import validateEmail from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const navigator = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError('Invalid Email');
+      return;
+    }
+
+    if(!password){
+      setError('Password is required');
+      return;
+    }
+
+    setError(null);
+
+    try {
+      const response = await axiosInstance.post('/login', {email: email, password: password});
+      if(response.data && response.data.acccesToken){
+        localStorage.setItem('token', response.data.acccesToken);
+        navigator('/dashboard');
+      }
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  }
+
+
   return (
     <div className='h-screen bg-cyan-50 overflow-hidden relative'>
+      <div className='login-ui-box'></div>
       <div className='container h-screen flex items-center justify-center px-20 mx-auto'>
         <div className='w-2/4 h-[90vh] flex items-end bg-cover rouded-lg p-10 z-50 bg-login-bg-img'>
           <div>
@@ -16,10 +54,15 @@ const Login = () => {
           </div>
         </div>
         <div className='w-2/4 h-[70vh] bg-white rounded-r-lg p-16 shadow-lg shadow-cyan-200/20'>
-          <form className='' action="" onSubmit={() => {}}>
+          <form className='' action="" onSubmit={handleLogin}>
             <h4 className='text-2xl font-semibold mb-7'>Login</h4>
-            <input type="text" placeholder='Email' className='input-box'/>
-            <PasswordInput />
+            <input type="text" placeholder='Email' className='input-box' value={email} onChange={({target}) => {
+              setEmail(target.value)
+            }}/>
+            <PasswordInput value={password} onChange={({target}) => {
+              setPassword(target.value)
+            }}/>
+            {error && <p className='text-xs text-red-500'>{error}</p>}
             <button type='submit' className='btn-primary'>Login</button>
             <p className='text-xs text-slate-500 text-center my-4'>Or</p>
             <button type='submit' className='btn-light btn-primary' onClick={() => {
--- Test/my-project/src/components/Input/PasswordInput.jsx
+++ Test/my-project/src/components/Input/PasswordInput.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
-const PasswordInput = (value, onChange, placeholder) => {
+const PasswordInput = ({ value, onChange, placeholder }) => {
+
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const togglePassword = () => {
+    setIsShowPassword(!isShowPassword);
+  }
+
   return (
     <div className='flex items-center bg-cyan-600/5 px-5 rounded mb-3'>
-      <input type="text" 
+      <input type={isShowPassword ? 'text' : 'password'} 
         value={value}
         placeholder={placeholder || "Password"}
+        onChange={onChange}
         className='w-full text-sm bg-transparent py-3 rounded outline-none'/>
+      {isShowPassword ?
+        <FaRegEye onClick={togglePassword} className='text-primary cursor-pointer'/>
+        :
+        <FaRegEyeSlash onClick={togglePassword} className='text-primary cursor-pointer'/>
+      }
+      
     </div>
   )
 }
